10da845e941fec69b689d01b0140da749014e828
Ira Horecka <ira89@icloud.com>
2020-05-14 21:53:20 -0700

    initial flask website with jQuery and background image

---
+++ static/script.js
@@ -0,0 +1,5 @@
+$(document).ready(function () {
+  $('h1.fade-in').css('visibility','visible').hide().fadeIn(1500);
+  $('h3').css('visibility','visible').hide().delay(500).fadeIn(1500);
+  $('li').css('visibility','visible').hide().delay(1200).fadeIn(1500);
+});

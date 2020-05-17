114eed09437924dc3eaea26acb2b8b5c421f5db2
Ira Horecka <ira89@icloud.com>
2020-05-17 13:58:54 -0700

    add favicon image, tweak fade-in on load, add fade-in/out on top scroll.

--- personalWebsite.py
+++ personalWebsite.py
@@ -9,40 +9,54 @@ def home():
     title = "Home Page"
     return render_template("home.html", title=title)
 
+
 @app.route("/about")
 def about():
     title = "About"
     return render_template("about.html", title=title)
+
+
+@app.route("/bikes")
+def bikes():
+    title = "Bike Gallery"
+    return render_template("bikes.html", title=title)
+
 
 @app.route("/projects")
 def coding_projects():
     title = "Coding Projects"
     return render_template("projects.html", title=title)
 
+
 @app.route("/projects/astree")
 def astree():
     title = "ASTree"
     return render_template("astree.html", title=title)
+
 
 @app.route("/projects/actransit")
 def actransit():
     title = "AC Transit"
     return render_template("actransit.html", title=title)
 
+
 @app.route("/projects/craigslist-mining")
 def cl_mining():
     title = "Craigslist Mining"
     return render_template("craigslistmining.html", title=title)
+
 
 @app.route("/projects/visuaudio")
 def visuaudio():
     title = "Visualize Audio"
     return render_template("visuaudio.html", title=title)
 
+
 @app.route("/projects/youtube2audio")
 def youtube2audio():
     title = "YouTube to Audio"
     return render_template("youtube2audio.html", title=title)
+
 
 @app.errorhandler(404)
 def not_found(e):
--- static/script.js
+++ static/script.js
@@ -1,13 +1,42 @@
 $(document).ready(function () {
-  var urlCurrent = window.location.href
+  fadeIn();
+});
+
+// Function to handle fade-out of text above when scrolling down
+$(window).on("load",function() {
+  function fade(pageLoad) {
+    var windowTop=$(window).scrollTop(), windowBottom=windowTop+$(window).innerHeight();
+    var min=0.3, max=1.0, threshold=0.01;
+    
+    $(".fade-scroll").each(function() {
+      /* Check the location of each desired element */
+      var objectHeight=$(this).outerHeight(), objectTop=$(this).offset().top, objectBottom=$(this).offset().top+objectHeight;
+      
+      /* Fade element in/out based on its visible percentage */
+      if (objectTop < windowTop) {
+        if (objectBottom > windowTop) {$(this).fadeTo(0,min+((max-min)*((objectBottom-windowTop)/objectHeight)));}
+        else if ($(this).css("opacity")>=min+threshold || pageLoad) {$(this).fadeTo(0,min);}
+      } else if ($(this).css("opacity")<=max-threshold || pageLoad) {$(this).fadeTo(0,max);}
+    });
+  } fade(true);
+  $(window).scroll(function(){fade(false);}); //fade elements on scroll
+});
+
+
+// A function to handle fade-in properties of webpage
+function fadeIn() {
+  var urlCurrent = window.location.href;
   var urlReference = document.referrer;
   var title = document.title
+  // all p-tags should be wrapped in div tag
   var titleFadeAttribute = {
     "Home Page": ['h1.fade-in', 'h3.fade-in', 'li'],
-    "AC Transit": ['h3.fade-in', 'img.fade-in'],
-    "YouTube to Audio": ['h3.fade-in', 'p.fade-in']
+    "About": ['h3.fade-in', 'div.fade-in'],
+    "AC Transit": ['h3.fade-in', 'div.fade-in'],
+    "YouTube to Audio": ['h3.fade-in', 'div.fade-in']
   };
 
+  // set default fade in attributes if none of special pages selected
   if (title in titleFadeAttribute) {
     htmlTags = titleFadeAttribute[title];
   } else {
@@ -28,14 +57,13 @@ $(document).ready(function () {
     htmlTags = htmlTags.slice(1,)
   } 
 
+  // Allow fade-in with delay
   var msDelay = 0;
   var msFadeIn = 800;
-
-  // Allow fade-in with delay
   for (tag in htmlTags) {
       $(htmlTags[tag]).css('visibility','visible').hide().delay(msDelay).fadeIn(msFadeIn);
       if (title != 'Coding Projects') {
         msDelay += 300;
       }
   }
-});
+}
